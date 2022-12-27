ea4737e0f5e5278d3a1aca1a7e8719e8bb805666
Zach <zachlandis91@gmail.com>
2022-12-27 14:55:54 -0700

    commit tree e00691c5af12724d954762572b95d36cfaf9508c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    let textId = document.getElementById('text');
+    textId.textContent = "This is really cool!";
+});    
